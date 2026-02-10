14c0a337fe11596fb1a3b5bad2fc23a048da13d8
robjlyons <81177492+robjlyons@users.noreply.github.com>
2026-02-10 21:56:10 +0000

    Update core.asm

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -173,6 +173,8 @@ StartBattle:
 	ld a, 1
 	ld [wWildEncounterCanCatch], a
 .skipWildCatchInit
+	ld a, 1
+	ld [wWildEncounterCanCatch], a
 	ld a, [wIsInBattle]
 	dec a ; is it a trainer battle?
 	call nz, EnemySendOutFirstMon ; if it is a trainer battle, send out enemy mon
