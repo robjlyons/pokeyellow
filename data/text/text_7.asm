cf4a8faf013759ec69bb556d7ec2bccc5bd6ed83
robjlyons <81177492+robjlyons@users.noreply.github.com>
2026-02-10 10:00:31 +0000

    Restore canonical WRAM stack layout

--- data/text/text_7.asm
+++ data/text/text_7.asm
@@ -13,6 +13,14 @@ _PokemonFaintedText::
 	line "fainted!"
 	done
 
+_PokemonRanAwayBadTrainingText::
+	text_ram wNameBuffer
+	text_start
+	line "ran away due"
+	cont "to your bad"
+	cont "training!"
+	prompt
+
 _PlayerBlackedOutText::
 	text "<PLAYER> is out of"
 	line "useable #MON!"
